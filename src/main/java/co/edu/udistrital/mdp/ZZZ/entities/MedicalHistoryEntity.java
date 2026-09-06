facfa6dc7e874be28adde5d1a20e2a757cb6f8d5
ADMZ <duvan4492@gmail.com>
2026-09-06 13:06:17 -0500

    Implementa capa de persistencia y entidades

--- src/main/java/co/edu/udistrital/mdp/ZZZ/entities/MedicalHistoryEntity.java
+++ src/main/java/co/edu/udistrital/mdp/ZZZ/entities/MedicalHistoryEntity.java
@@ -1,16 +1,25 @@
 package co.edu.udistrital.mdp.ZZZ.entities;
 
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Entity
 public class MedicalHistoryEntity extends BaseEntity {
-    
-    // Aquí van los atributos específicos:
-    // private String diagnosis;
-    // private String treatment;
-    
+
+    private String vaccine;
+    private String diseases;
+    private String treatment;
+    private Boolean sterilized;
+
+    @PodamExclude
+    @OneToOne
+    @JoinColumn(name = "pet_id")
+    private PetEntity pet;
 }
