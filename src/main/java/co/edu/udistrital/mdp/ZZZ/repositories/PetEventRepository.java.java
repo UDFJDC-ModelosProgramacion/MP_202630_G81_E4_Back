9aa52af7389cb442eac74a0341d36a815c0e32fb
Admz4492 <duvan4492@gmail.com>
2026-09-06 21:37:12 -0500

    Delete src/main/java/co/edu/udistrital/mdp/ZZZ/repositories/PetEventRepository.java.java

--- src/main/java/co/edu/udistrital/mdp/ZZZ/repositories/PetEventRepository.java.java
+++
@@ -1,9 +0,0 @@
-package co.edu.udistrital.mdp.ZZZ.repositories;
-
-import co.edu.udistrital.mdp.ZZZ.entities.PetEventEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PetEventRepository extends JpaRepository<PetEventEntity, Long> {
-}
