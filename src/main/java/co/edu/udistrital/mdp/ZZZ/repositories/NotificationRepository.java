94dd60f765a1177431cb3d9da88c5b912737f92b
Sebastian Candia <candiamarins@gmail.com>
2026-09-13 23:50:11 -0500

    Implemento services de User, Adopter y Message con reglas de negocio

--- src/main/java/co/edu/udistrital/mdp/ZZZ/repositories/NotificationRepository.java
+++ src/main/java/co/edu/udistrital/mdp/ZZZ/repositories/NotificationRepository.java
@@ -1,10 +1,14 @@
 package co.edu.udistrital.mdp.ZZZ.repositories;
 
+import java.util.List;
+
 import co.edu.udistrital.mdp.ZZZ.entities.NotificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
-    
+
+    List<NotificationEntity> findByUserIdAndReadFalse(Long userId);
+
 }
--- src/main/java/co/edu/udistrital/mdp/ZZZ/repositories/UserRepository.java
+++ src/main/java/co/edu/udistrital/mdp/ZZZ/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package co.edu.udistrital.mdp.ZZZ.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import co.edu.udistrital.mdp.ZZZ.entities.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
 
 }
